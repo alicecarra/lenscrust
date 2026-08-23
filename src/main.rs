f55f7cb5484d49cc207d2bf5c7c00d0313fbaf73
Alice Carra <aalicecarra@gmail.com>
2026-08-22 21:11:55 -0300

    loading images

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,59 @@ fn main() -> eframe::Result {
 #[derive(Default)]
 struct MyApp {
     texture: Option<egui::TextureHandle>,
+    texture_name: Option<String>,
+}
+
+impl MyApp {
+    fn load_texture(&mut self, ui: &mut egui::Ui) {
+        let current_dir = std::env::current_dir().unwrap();
+
+        let Some(image_path) = rfd::FileDialog::new()
+            .add_filter("image", &["jpg", "png"])
+            .set_directory(&current_dir)
+            .pick_file()
+        else {
+            return;
+        };
+
+        self.texture_name = Some(image_path.file_name().unwrap().display().to_string());
+
+        let loaded_image: image::DynamicImage =
+            ImageReader::open(&image_path).unwrap().decode().unwrap();
+
+        let size = [
+            loaded_image.width() as usize,
+            loaded_image.height() as usize,
+        ];
+        let rgba = loaded_image.to_rgba8();
+        let image_to_render =
+            egui::ColorImage::from_rgba_unmultiplied(size, rgba.as_flat_samples().as_slice());
+
+        self.texture = Some(ui.ctx().load_texture(
+            self.texture_name.as_deref().unwrap_or("image"),
+            image_to_render,
+            Default::default(),
+        ));
+    }
 }
 
 impl eframe::App for MyApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        let texture = self.texture.get_or_insert_with(|| {
-            let loaded_image: image::DynamicImage =
-                ImageReader::open("test_images/Underwater_53k.jpg")
-                    .unwrap()
-                    .decode()
-                    .unwrap();
-            let size = [
-                loaded_image.width() as usize,
-                loaded_image.height() as usize,
-            ];
-            let rgba = loaded_image.to_rgba8();
-            let image_to_render =
-                egui::ColorImage::from_rgba_unmultiplied(size, rgba.as_flat_samples().as_slice());
-            ui.ctx()
-                .load_texture("underwater", image_to_render, Default::default())
+        egui::MenuBar::new().ui(ui, |ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("Load Image").clicked() {
+                    self.load_texture(ui);
+                } else if ui.button("Quit").clicked() {
+                    ui.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+            });
         });
 
         egui::CentralPanel::default().show(ui, |ui| {
             egui::ScrollArea::both().show(ui, |ui| {
-                ui.image(&*texture).on_hover_text_at_pointer("foobar");
+                if let Some(texture) = &self.texture {
+                    ui.image(texture);
+                }
             });
         });
     }
